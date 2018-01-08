8340ea87726c068b3a017e400d6f476612585b1e
Fahima Islam <ffiqnf@mst.edu>
2018-01-08 16:46:42 -0500

    Refs #10 re-edit test

--- tests/models/test_tabulatedmodel.py
+++ tests/models/test_tabulatedmodel.py
@@ -10,16 +10,26 @@ from qef.models.tabulatedmodel import TabulatedModel
 
 def test_tabulation():
     x_sim = np.arange(-1.0, 1.0, 0.0003)  # energy domain, in meV
+    y_sim = lorentzian(x_sim , amplitude=1, center=0, sigma=0.042)
     intensity = 42.0
     peak_center = 0.0002
     x_exp = np.arange(-0.1, 0.5, 0.0004)
     y_exp = lorentzian(x_exp - 0, amplitude=intensity, center=peak_center, sigma=0.042)
 
-    model = TabulatedModel(x_sim, 0)
-    params = model.guess(x_exp, y_exp, 0.042)
+    model = TabulatedModel(x_sim,y_sim)
+    params = model.guess(x_exp, y_exp)
     fit = model.fit(y_exp, params, x=x_exp, fit_kws={'nan_policy': 'omit'})
 
     print (fit.params)
 
 if __name__ == '__main__':
     pytest.main([os.path.abspath(__file__)])
+
+
+
+
+
+
+
+
+
