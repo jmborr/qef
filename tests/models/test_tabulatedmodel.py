85cc4d066509d027b1991d4f777e0f95b8912a0b
Fahima Islam <ffiqnf@mst.edu>
2018-01-17 12:52:25 -0500

    Refs #10 test modified

--- tests/models/test_tabulatedmodel.py
+++ tests/models/test_tabulatedmodel.py
@@ -10,7 +10,7 @@ from qef.models.tabulatedmodel import TabulatedModel
 
 def test_tabulation():
     x_sim = np.arange(-1.0, 1.0, 0.0003)  # energy domain, in meV
-    y_sim = lorentzian(x_sim , amplitude=1, center=0, sigma=0.042)
+    y_sim = lorentzian(x_sim, amplitude=1, center=0, sigma=0.042)
     intensity = 42.0
     peak_center = 0.0002
     x_exp = np.arange(-0.1, 0.5, 0.0004)
