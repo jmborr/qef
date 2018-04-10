1d8b31bc50c8d298d1041dc54438ee1d43350ec4
Jose Borreguero <borreguero@gmail.com>
2018-04-09 20:27:56 -0400

    Refs #37 bump version

--- HISTORY.rst
+++ HISTORY.rst
@@ -4,6 +4,7 @@ History
 
 0.2.0 ()
 --------
+* Notebook for global fitting (PR #40)
 * Load Mantid Nexus data (PR #38)
 * Tabulated resolution model (PR #43)
 
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='qef',
-    version='0.1.0',
+    version='0.2.0',
     description="quasielastic fitting",
     long_description=readme + '\n\n' + history,
     author="Jose Borreguero",
