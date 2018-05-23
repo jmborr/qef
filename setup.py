be5634059d4e3b135e9d454193fe95173482eaf9
Jose Borreguero <borreguero@gmail.com>
2018-05-23 17:01:41 -0400

    Refs #55 require ipywidgets as dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'h5py', 'lmfit', 'numpy', 'matplotlib', 'scipy', 'six', 'traitlets'
+    'h5py', 'lmfit', 'numpy', 'matplotlib', 'scipy', 'six', 'ipywidgets'
     # TODO: put package requirements here
 ]
 
