bd5ee0383d787c1caf1259f6912b99400d624162
Jose Borreguero <borreguero@gmail.com>
2018-05-30 16:21:16 -0400

    Refs #57 remove unused import

--- tests/widgets/test_parameter.py
+++ tests/widgets/test_parameter.py
@@ -4,7 +4,6 @@
 from __future__ import (absolute_import, division, print_function)
 
 import os
-import numpy as np
 import pytest
 import lmfit
 from qef.widgets.parameter import ParameterWithTraits, ParameterWidget
