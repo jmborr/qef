fb2fa1e8cdd9bec84d6f1bc9084ab62a6f90f593
Jose Borreguero <borreguero@gmail.com>
2018-05-30 16:12:01 -0400

    Refs #57 added more links

--- qef/widgets/parameter.py
+++ qef/widgets/parameter.py
@@ -56,23 +56,60 @@ class ParameterWithTraits(lmfit.Parameter, traitlets.HasTraits):
                     lmfit.Parameter.__setattr__(self, other_key, value)
 
 
-class ParameterWidget(ipyw.HBox):
+class ParameterWidget(ipyw.Box):
     r"""One possible representation of a fitting parameter"""
     inf = float('inf')
 
-    def __init__(self):
-        self.value = ipyw.FloatText(description='value:', value=0, allow_none=True)
-        self.nomin = ipyw.Checkbox(description='-inf', value=True)
-        self.min = ipyw.FloatText(description='min:', value=-self.inf)
-        self.nomax = ipyw.Checkbox(description='inf', value=True)
-        self.max = ipyw.FloatText(description='max:', value=self.inf)
-        self.vary = ipyw.Checkbox(description='vary', value=True)
-        self.expr = ipyw.Text(description='constrain:', value='')
-        self.elements = [self.nomin, self.min, self.value,
-                         self.nomax, self.max, self.vary, self.expr]
+    def __init__(self, show_header=True):
+        def el_ly(w):
+            return dict(width='{}px'.format(w), margin='0px')
+
+        # minimum block
+        self.nomin = ipyw.Checkbox(value=True, layout=el_ly(125))
+        self.min = ipyw.FloatText(value=-self.inf, layout=el_ly(165))
+        box_ly = dict(border='1px solid black', display='flex', margin='0px',
+                      flex_flow='row', width='290px')
+        self.minbox = ipyw.Box([self.nomin, self.min], layout=box_ly)
+
+        # value element
+        self.value = ipyw.FloatText(value=0, layout=el_ly(170))
+
+        # maximum block
+        self.nomax = ipyw.Checkbox(value=True, layout=el_ly(125))
+        self.max = ipyw.FloatText(value=self.inf, layout=el_ly(165))
+        self.maxbox = ipyw.Box([self.nomax, self.max], layout=box_ly)
+
+        # constraints block
+        self.vary = ipyw.Checkbox(value=True, layout=el_ly(125))
+        self.expr = ipyw.Text(value='', continuous_update=False,
+                              layout=el_ly(275))
+
+        # array elements in an horizontal block
+        self.elements = [self.minbox, self.value, self.maxbox,
+                         self.vary, self.expr]
+
+        # Header labels
+        self.header = None
+        if show_header is True:
+            d_lbs = (('-inf', 125), ('min', 165), ('value', 170),
+                     ('inf', 125), ('max', 165), ('vary', 125),
+                     ('expression', 275))
+            l_lbs = [ipyw.Label(k, layout=el_ly(v)) for (k, v) in d_lbs]
+            box_ly = dict(display='flex', margin='0px', border='solid')
+            self.header = ipyw.HBox(l_lbs, layout=box_ly)
+
         self.initialize_callbacks()
-        self.initialize_layouts()
-        super(ParameterWidget, self).__init__(self.elements)
+
+        if self.header is None:
+            box_ly = dict(display='flex', margin='0px', border='solid',
+                          flex_flow='row')
+            super(ParameterWidget, self).__init__(self.elements, layout=box_ly)
+        else:
+            box_ly = dict(display='flex', margin='0px', border='solid')
+            b_els = ipyw.HBox(self.elements, layout=box_ly)
+            box_ly.update({'flex_flow': 'column'})
+            super(ParameterWidget, self).__init__([self.header, b_els],
+                                                  layout=box_ly)
 
     def initialize_callbacks(self):
         r"""Register callbacks to sync elements"""
@@ -81,6 +118,8 @@ class ParameterWidget(ipyw.HBox):
         self.nomax.observe(self.nomax_changed, 'value', 'change')
         self.max.observe(self.max_changed, 'value', 'change')
         self.value.observe(self.value_changed, 'value', 'change')
+        self.vary.observe(self.vary_changed, 'value', 'change')
+        self.expr.observe(self.expr_changed, 'value', 'change')
 
     def nomin_changed(self, change):
         r"""Set min to -infinity if nomin is checked"""
@@ -123,5 +162,12 @@ class ParameterWidget(ipyw.HBox):
         elif change.new > self.max.value:
             self.value.value = self.max.value
 
-    def initialize_layouts(self):
-        r"""Prettify the look"""
+    def vary_changed(self, change):
+        r"""enable/disable boundaries and value"""
+        for w in (self.nomin, self.min, self.value, self.nomax, self.max,
+                  self.expr):
+            w.disabled = not change.new
+
+    def expr_changed(self, change):
+        r"""vary/no-vary boundaries and values"""
+        self.vary.value = True if change.new == '' else False
